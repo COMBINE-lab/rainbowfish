aaef2554db8fc045c93f29de141023c65f186b9a
Fatemeh <sfmodarres@gmail.com>
2017-04-26 10:53:20 -0400

    Change private member colorCnt to colorCnt_

--- rb-query.cpp
+++ rb-query.cpp
@@ -2,31 +2,36 @@
 #include "rb-query.hpp"
 
 template <class T1, class T2, class T3>
-ColorDetector<T1, T2, T3>::ColorDetector(std::string dir, size_t colorCnt) :
-		eqT(dir + "/eqTable", false), A(dir + "/lbl", false), b(dir + "/rnk", true) {
-	this->colorCnt = colorCnt;
+ColorDetector<T1, T2, T3>::ColorDetector(std::string dir, uint64_t colorCnt) :
+		A(dir + "/lbl", false), b(dir + "/rnk", true), eqT(dir + "/eqTable", false) {
+	colorCnt_ = colorCnt;
 	prevEdge_=std::numeric_limits<uint64_t>::max();
 }
 
 template <class T1, class T2, class T3>
 bool ColorDetector<T1, T2, T3>::prevContains_(unsigned int color) {
-	return eqT[colorCnt*prevColor_+ color];
+	return eqT[colorCnt_*prevColor_+ color];
 }
 
 template <class T1, class T2, class T3>
 bool ColorDetector<T1, T2, T3>::contains(unsigned int color, uint64_t edge) {
 	if (edge == prevEdge_) { return prevContains_(color); }
+//	std::cout<<edge<< " & "<<prevEdge_<< "  ";
+//	std::cout<<" q ";
 	uint64_t start = b.select(edge);
 	uint64_t end = b.select(edge+1);//todo: what if the edge is the last one?
+//	std::cout<<start<<","<<end;
 	uint64_t colorIdx = A.getInt(start, end-start);
+//	std::cout<<"->"<<colorIdx;
 	prevEdge_ = edge;
 	prevColor_ = colorIdx;
-	return eqT[colorCnt*colorIdx + color];
+//	std::cout<<" c6"<<eqT[colorIdx*colorCnt_+6]<<" ";
+	return eqT[colorCnt_*colorIdx + color];
 }
 
 template <class T1, class T2, class T3>
 size_t ColorDetector<T1, T2, T3>::getColorCnt() {
-	return colorCnt;
+	return colorCnt_;
 }
 
 template class ColorDetector<RBVec, RBVec, RBVec>;
--- rb-query.hpp
+++ rb-query.hpp
@@ -9,14 +9,14 @@ class ColorDetector {
 	T1 A;
 	T2 b;
 	T3 eqT;
-	size_t colorCnt;
+	uint64_t colorCnt_;
 	uint64_t prevEdge_;
 	uint64_t prevColor_;
 	bool prevContains_(unsigned int color);
   public:
-	ColorDetector(std::string dir, size_t colorCnt); // todo: we should keep count of colors or k-mers in top of the file
+	ColorDetector(std::string dir, uint64_t colorCnt); // todo: we should keep count of colors or k-mers in top of the file
 	bool contains(unsigned int color, uint64_t edge);
-	size_t getColorCnt();
+	uint64_t getColorCnt();
   //private:
 	//boost::dynamic_bitset<> readBitset(std::string filename);
 	//BitmapPoppy* readRSBitset(std::string filename);	
