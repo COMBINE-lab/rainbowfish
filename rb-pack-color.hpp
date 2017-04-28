f5e2c7e3221d074e1e7552aec605bd0b66d4b660
Rob Patro <rob@newton>
2017-04-28 14:25:36 -0400

    remove rb-pack-color dep on pack-color.hpp

---
+++ rb-pack-color.hpp
@@ -0,0 +1,20 @@
+#ifndef PACK_COLOR_HPP
+#define PACK_COLOR_HPP
+
+int getMilliCount();
+int getMilliSpan(int nTimeStart);
+typedef struct p
+{
+    std::string input_filename = "";
+    int num_colors;
+    unsigned long long m;
+    unsigned long long n;
+    std::string output_prefix = "";
+    std::string res_dir= "";
+} parameters_t;
+
+
+void parse_arguments(int argc, char **argv, parameters_t & params);
+
+
+#endif
