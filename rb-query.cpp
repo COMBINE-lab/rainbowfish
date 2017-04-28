96245ec98e4d45eabe3ace4bd59494ece13ccceb
Fatemeh <sfmodarres@gmail.com>
2017-04-28 14:44:55 -0400

    Add Time Statustic calculator to ColorDetector.

--- rb-query.cpp
+++ rb-query.cpp
@@ -6,27 +6,35 @@ ColorDetector<T1, T2, T3>::ColorDetector(std::string dir, uint64_t colorCnt) :
 		A(dir + "/lbl", false), b(dir + "/rnk", true), eqT(dir + "/eqTable", false) {
 	colorCnt_ = colorCnt;
 	prevEdge_=std::numeric_limits<uint64_t>::max();
+	select_t = 0;
+	getInt_t = 0;
+	getColor_t = 0;
 }
 
 template <class T1, class T2, class T3>
 bool ColorDetector<T1, T2, T3>::prevContains_(unsigned int color) {
-	return eqT[colorCnt_*prevColor_+ color];
+	//uint64_t st = getMilliCountt();
+	bool res = eqT[colorCnt_*prevColor_+color];
+	//getColor_t += getMilliSpant(st);
+	return res;
 }
 
 template <class T1, class T2, class T3>
 bool ColorDetector<T1, T2, T3>::contains(unsigned int color, uint64_t edge) {
 	if (edge == prevEdge_) { return prevContains_(color); }
-//	std::cout<<edge<< " & "<<prevEdge_<< "  ";
-//	std::cout<<" q ";
+	//uint64_t st = getMilliCountt();
 	uint64_t start = b.select(edge);
 	uint64_t end = b.select(edge+1);//todo: what if the edge is the last one?
-//	std::cout<<start<<","<<end;
+	//select_t += getMilliSpant(st);
+	//st = getMilliCountt();
 	uint64_t colorIdx = A.getInt(start, end-start);
-//	std::cout<<"->"<<colorIdx;
+	//getInt_t += getMilliSpant(st);
 	prevEdge_ = edge;
 	prevColor_ = colorIdx;
-//	std::cout<<" c6"<<eqT[colorIdx*colorCnt_+6]<<" ";
-	return eqT[colorCnt_*colorIdx + color];
+	//st = getMilliCountt();
+	bool res = eqT[colorCnt_*colorIdx+color];
+	//getColor_t += getMilliSpant(st);
+	return res;
 }
 
 template <class T1, class T2, class T3>
@@ -34,27 +42,16 @@ size_t ColorDetector<T1, T2, T3>::getColorCnt() {
 	return colorCnt_;
 }
 
+template <class T1, class T2, class T3>
+void ColorDetector<T1, T2, T3>::printStatistics() {
+	std::cout<<"------------Some STATISTICS-------------\n";
+	std::cout<<"		select timing: "<<select_t<<" ms";
+	std::cout<<"		getColor timing: "<<getColor_t<<" ms";
+	std::cout<<"		getInt timing: "<<getInt_t<<" ms"<<std::endl;
+}
+
 template class ColorDetector<RBVec, RBVec, RBVec>;
 template class ColorDetector<RBVecCompressed, RBVecCompressed, RBVecCompressed>;
 template class ColorDetector<RBVec, RBVecCompressed, RBVecCompressed>;
 template class ColorDetector<RBVec, RBVec, RBVecCompressed>;
 
-/*int main(int, char*[]) {
-	ColorDetector<RBVecCompressed> cd("A.bitvec", "B.bitvec", "eqTable.bitvec", 6);
-	std::ofstream f{"ours.res"};
-	size_t num_colors = 6;
-	uint64_t edge_cnt = 9256522;
-	uint64_t edge_cnt = 12;
-	for (uint64_t edge = 0; edge < edge_cnt; edge++) {
-		short our_color_mask = 0;
-		std::cout<<edge<<":";
-		for (size_t c = 0; c < num_colors; c++) {
-			std::cout<<cd.contains(c, edge);
-			our_color_mask |= (cd.contains(c, edge) << c);
-		}
-		std::cout<< " --> "<<our_color_mask<<"\n";
-		f<<our_color_mask<<std::endl;
-	}
-	f.close();
-	return EXIT_SUCCESS;
-}*/
--- rb-query.hpp
+++ rb-query.hpp
@@ -3,12 +3,20 @@
 
 #include "rb-vec.hpp"
 
+#include <sys/timeb.h>
+
+
+
 template <class T1, class T2, class T3>
 class ColorDetector {
   private:
 	T1 A;
 	T2 b;
 	T3 eqT;
+	uint64_t select_t;
+	uint64_t getInt_t;
+	uint64_t getColor_t;
+	
 	uint64_t colorCnt_;
 	uint64_t prevEdge_;
 	uint64_t prevColor_;
@@ -17,6 +25,23 @@ class ColorDetector {
 	ColorDetector(std::string dir, uint64_t colorCnt); // todo: we should keep count of colors or k-mers in top of the file
 	bool contains(unsigned int color, uint64_t edge);
 	uint64_t getColorCnt();
+	void printStatistics();
+int getMilliCountt(){
+  timeb tb;
+  ftime(&tb);
+  int nCount = tb.millitm + (tb.time & 0xfffff) * 1000;
+  return nCount;
+}
+
+
+int getMilliSpant(int nTimeStart){
+  int nSpan = getMilliCountt() - nTimeStart;
+  if(nSpan < 0)
+    nSpan += 0x100000 * 1000;
+  return nSpan;
+}
+
+
   //private:
 	//boost::dynamic_bitset<> readBitset(std::string filename);
 	//BitmapPoppy* readRSBitset(std::string filename);	
